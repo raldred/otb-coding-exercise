8785bf2d1d4d6841c06ec0f31ddd6f09c125863c
Rob Aldred <raldred@gmail.com>
2016-11-26 19:53:12 +0000

    Added the all specs Added simple shell script Parse jobs coming in and initial dependency checks

--- lib/job_sequencer.rb
+++ lib/job_sequencer.rb
@@ -1,11 +1,51 @@
+class SequencerException < StandardError
+	def initialize(msg = "Sequencer Exception")
+		super
+	end
+end
+
+
 class JobSequencer
 
 	def initialize
-		
+		@jobs_arr = []
 	end
 
-	def process job_list
-		[]
+	def process job_list_string
+		@jobs_arr = parse_job_string(job_list_string)
+		raise SequencerException, "Jobs cannot depend on themselves" if have_self_dependency?
+		raise SequencerException, "Jobs cannot have circular dependencies" if have_circular_dependencies?
 	end
+
+
+	private
+
+		attr_reader :jobs_arr
+
+		# split job input into job with it's dependency
+		def parse_job_string job_list_string
+			# split input lines
+			arr = job_list_string.split(/\r?\n/)
+			# strip all whitespace from lines
+			arr.map! {|l| l.gsub(/\s+/, '') }
+			# strip pointers from lines
+			arr.map! {|l| l.gsub(/=>/, '') }
+			# get rid of empty lines
+			arr.reject!(&:empty?)
+			arr
+		end
+
+		def jobs_with_dep_arr
+			# only jobs with dependency have 2 chars
+			jobs_arr.select { |j| j.length == 2 }
+		end
+
+		def have_self_dependency?
+			jobs_with_dep_arr.any? { |j| j[0] == j[-1] }
+		end
+
+		def have_circular_dependencies?
+			false
+		end
 	
 end
--- spec/job_sequencer_spec.rb
+++ spec/job_sequencer_spec.rb
@@ -53,7 +53,52 @@ describe JobSequencer do
 					}
 				end
 			it 'should return all three jobs with c before b' do
-				expect(subject.process(joblist)).to eq(['a','c','b'])
+				expect(subject.process(joblist)).to eq(['c', 'a', 'b'])
+			end
+		end
+
+		context 'given multiple jobs with a dependenies' do
+			let(:joblist) do
+					%Q{
+						a =>
+						b => c
+						c => f
+						d => a
+						e => b
+						f =>
+					}
+				end
+			it 'should return all jobs with f before c, c before b, b before e and a before d' do
+				expect(subject.process(joblist)).to eq(['b', 'a', 'f', 'c', 'd', 'e'])
+			end
+		end
+
+		context 'given a job that depends on itself' do
+			let(:joblist) do
+					%Q{
+						a =>
+						b =>
+						c => c
+					}
+				end
+			it 'should raise an error' do
+				expect { subject.process(joblist) }.to raise_error(SequencerException, "Jobs cannot depend on themselves")
+			end
+		end
+
+		context 'given multiple jobs that cycle' do
+			let(:joblist) do
+					%Q{
+						a =>
+						b => c
+						c => f
+						d => a
+						e =>
+						f => b
+					}
+				end
+			it 'should raise an error' do
+				expect { subject.process(joblist) }.to raise_error(SequencerException, "Jobs cannot have circular dependencies")
 			end
 		end
 
